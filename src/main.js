3ed1ac280c3a4b300caafc82b55a7e69da182064
opencoursesz <opencoursesz@163.com>
2020-04-29 10:56:11 +0800

    SSR base complete

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
+/* eslint-disable import/prefer-default-export */
 import Vue from 'vue'
+import createRouter from '@/router'
+import createStore from '@/store'
 import App from './App.vue'
-import router from './router'
-import store from './store'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app')
+export function createApp() {
+  const router = createRouter()
+  const store = createStore()
+  const app = new Vue({
+    router,
+    store,
+    render: (h) => h(App),
+  })
+  return { app, router, store }
+}
